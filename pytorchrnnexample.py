ba284768f49e7dc4cf99c392f7eaf304990ee8bf
Emily <emilyhwhyms1@gmail.com>
2025-07-18 14:17:59 -0700

    update

--- pytorchrnnexample.py
+++ pytorchrnnexample.py
@@ -17,10 +17,8 @@ import torchvision.transforms as transforms
 train = pd.read_csv(r"/home/emily/Downloads/train.csv",dtype = np.float32)
 test = pd.read_csv(r"/home/emily/Downloads/test.csv",dtype = np.float32)
 targets_numpy = train.label.values
-features_numpy = train.loc[:,train.columns != "label"].values/255 # normalization
+features_numpy = train.loc[:,train.columns != "label"].values/255
 
-
-# train test split. Size of train data is 80% and size of test data is 20%. 
 features_train, features_test, targets_train, targets_test = train_test_split(features_numpy,
                                                                              targets_numpy,
                                                                              test_size = 0.2,
@@ -34,25 +32,14 @@ targetsTest = torch.from_numpy(targets_test).type(torch.LongTensor)
 class RNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(RNNModel, self).__init__()
-        
-        # Number of hidden dimensions
         self.hidden_dim = hidden_dim
-        
-        # Number of hidden layers
         self.layer_dim = layer_dim
-        
         # RNN
         self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
-        
-        # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
     
     def forward(self, x):
-        
-        # Initialize hidden state with zeros
         h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
-            
-        # One time step
         out, hn = self.rnn(x, h0)
         out = self.fc(out[:, -1, :]) 
         return out
@@ -72,17 +59,17 @@ train_loader = DataLoader(train, batch_size = batch_size, shuffle = False)
 test_loader = DataLoader(test, batch_size = batch_size, shuffle = False)
     
 # Create RNN
-input_dim = 28    # input dimension
-hidden_dim = 100  # hidden layer dimension
-layer_dim = 1     # number of hidden layers
-output_dim = 10   # output dimension
+input_dim = 28  
+hidden_dim = 100  
+layer_dim = 1
+output_dim = 10
 
 model = RNNModel(input_dim, hidden_dim, layer_dim, output_dim)
 
 # Cross Entropy Loss 
 error = nn.CrossEntropyLoss()
 
-# SGD Optimizer
+# Optimizer could also be Adam
 learning_rate = 0.05
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
@@ -96,39 +83,24 @@ for epoch in range(num_epochs):
 
         train  = Variable(images.view(-1, input_dim, seq_dim))
         labels = Variable(labels)
-            
-        # Clear gradients
         optimizer.zero_grad()
-        
         # Forward propagation
         outputs = model(train)
-        
         # Calculate softmax and ross entropy loss
         loss = error(outputs, labels)
-        
-        # Calculating gradients
         loss.backward()
-        
-        # Update parameters
         optimizer.step()
         
         count += 1
         
         if count % 250 == 0:
-            # Calculate Accuracy         
             correct = 0
             total = 0
-            # Iterate through test dataset
             for images, labels in test_loader:
                 images = Variable(images.view(-1, seq_dim, input_dim))
-                
-                # Forward propagation
                 outputs = model(images)
-                
-                # Get predictions from the maximum value
                 predicted = torch.max(outputs.data, 1)[1]
-                
-                # Total number of labels
+                # num of labels
                 total += labels.size(0)
                 
                 correct += (predicted == labels).sum()
@@ -142,14 +114,12 @@ for epoch in range(num_epochs):
             if count % 500 == 0:
                 # Print Loss
                 print('Iteration: {}  Loss: {}  Accuracy: {} %'.format(count, loss.data, accuracy))
-# visualization loss 
 plt.plot(iteration_list,loss_list)
 plt.xlabel("Number of iteration")
 plt.ylabel("Loss")
 plt.title("RNN: Loss vs Number of iteration")
 plt.show()
 
-# visualization accuracy 
 plt.plot(iteration_list,accuracy_list,color = "red")
 plt.xlabel("Number of iteration")
 plt.ylabel("Accuracy")
